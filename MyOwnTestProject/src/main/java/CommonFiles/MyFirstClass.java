a737093bd861c61bb9d99367952ffa7bffdaf636
Shilps <Shilps@LAPTOP-AQ5CIB5M>
2021-02-26 12:15:42 +0530

    first commit of the project

---
+++ MyOwnTestProject/src/main/java/CommonFiles/MyFirstClass.java
@@ -0,0 +1,16 @@
+package CommonFiles;
+
+public class MyFirstClass {
+	
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) 
+	{
+		MyFirstClass mf = new MyFirstClass();
+		mf.display();
+	}
+
+}
