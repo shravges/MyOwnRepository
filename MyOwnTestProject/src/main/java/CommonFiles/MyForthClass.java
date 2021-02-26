83acbb50817f2bdfb512ba683b595ff1807f2e27
Shilps <Shilps@LAPTOP-AQ5CIB5M>
2021-02-26 17:30:30 +0530

    created new branch via eclipse

---
+++ MyOwnTestProject/src/main/java/CommonFiles/MyForthClass.java
@@ -0,0 +1,16 @@
+package CommonFiles;
+
+public class MyForthClass {
+	
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) 
+	{
+		MyForthClass mf = new MyForthClass();
+		mf.display();
+	}
+
+}
