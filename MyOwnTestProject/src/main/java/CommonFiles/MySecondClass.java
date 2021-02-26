a07ceeb2fd1aa2398f638f94dc2239035b3a4438
Shilps <Shilps@LAPTOP-AQ5CIB5M>
2021-02-26 12:22:29 +0530

    added second class file

---
+++ MyOwnTestProject/src/main/java/CommonFiles/MySecondClass.java
@@ -0,0 +1,16 @@
+package CommonFiles;
+
+public class MySecondClass {
+	
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) 
+	{
+		MySecondClass mf = new MySecondClass();
+		mf.display();
+	}
+
+}
